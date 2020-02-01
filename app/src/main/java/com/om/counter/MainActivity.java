b3b83d3c712c81b0d6ffaa6d4548d538f4f8c2b3
Maxim Obrubov <m.obrubov@yandex.ru>
2020-02-02 00:25:03 +0400

    counter logic applied

--- app/src/main/java/com/om/counter/MainActivity.java
+++ app/src/main/java/com/om/counter/MainActivity.java
@@ -4,20 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int counter = 0;
+    TextView indicator;
 
     public void increaseCounter(View view) {
-        // выводим сообщение
-        Toast.makeText(this, "Counter increased", Toast.LENGTH_SHORT).show();
+        counter++;
+        Toast.makeText(this, String.valueOf(counter), Toast.LENGTH_SHORT).show();
+        this.indicator.setText(String.valueOf(counter));
+    }
+
+    public void decreaseCounter(View view) {
+        if (counter > 0) counter--;
+        this.indicator.setText(String.valueOf(counter));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.indicator = (TextView) findViewById(R.id.textview);
     }
 
 
