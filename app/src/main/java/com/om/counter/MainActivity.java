ed26abb0693cddf97ddd1298027e159d7df05e0e
Maxim Obrubov <m.obrubov@yandex.ru>
2020-02-04 19:24:53 +0400

    initial set preference

--- app/src/main/java/com/om/counter/MainActivity.java
+++ app/src/main/java/com/om/counter/MainActivity.java
@@ -1,9 +1,17 @@
 package com.om.counter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,9 +20,12 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
 
+    int VIBRATION_TIME = 100;
     int counter = 0;
+    boolean useHardware;
+    boolean useVibration;
     TextView indicator;
 
     /**
@@ -35,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     /**
-     * Redefine carret behaviour
+     * Redefine volume curret behaviour
      * @param event
      * @return boolean is success
      */
@@ -44,58 +55,100 @@ public class MainActivity extends AppCompatActivity {
         int action = event.getAction();
         int keyCode = event.getKeyCode();
 
-
+        Toast.makeText(this, String.valueOf(this.useHardware), Toast.LENGTH_SHORT).show();
 
         switch (keyCode) {
-            case KeyEvent.KEYCODE_VOLUME_UP:
-                if (action == KeyEvent.ACTION_UP) increaseCounter();
-                return true;
-            case KeyEvent.KEYCODE_VOLUME_DOWN:
-                if (action == KeyEvent.ACTION_DOWN) decreaseCounter();
-                return true;
-            default:
-                return super.dispatchKeyEvent(event);
+        case KeyEvent.KEYCODE_VOLUME_UP:
+            if (action == KeyEvent.ACTION_UP && this.useHardware) {
+                increaseCounter();
+            }
+            return true;
+        case KeyEvent.KEYCODE_VOLUME_DOWN:
+            if (action == KeyEvent.ACTION_DOWN && this.useHardware) {
+                decreaseCounter();
+            }
+            return true;
+        default:
+            return super.dispatchKeyEvent(event);
         }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater findMenuItems = getMenuInflater();
-        findMenuItems.inflate(R.menu.main_menu, menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
         switch (item.getItemId()) {
-            case R.id.options:
-                Toast.makeText(this, "Options", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
-                return true;
-            case R.id.about:
-                Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
-//TODO: about app xml
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        case R.id.options:
+            startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            return true;
+        case R.id.about:
+            Toast.makeText(this, "About", Toast.LENGTH_SHORT).show();
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
         }
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.indicator = (TextView) findViewById(R.id.textview);
+        setupSharedPreferences();
+
+        Toast.makeText(this, "On create", Toast.LENGTH_SHORT).show();
     }
+
+
+    private void setupSharedPreferences() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+//        TODO: check here to find why options are reset
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        if (key.equals("hardware_buttons")) {
+            this.useHardware = sharedPreferences.getBoolean("hardware_buttons", true);
+            Toast.makeText(this, key + " " + sharedPreferences.getBoolean("hardware_buttons", true) + " " + this.useHardware, Toast.LENGTH_SHORT).show();
+        } else if (key.equals("vibration")) {
+            Toast.makeText(this, key + sharedPreferences.getBoolean("vibration", true), Toast.LENGTH_SHORT).show();
+            this.useVibration = sharedPreferences.getBoolean("vibration", true);
+        }
+
+    }
+
 
     private void increaseCounter() {
         this.counter++;
         this.indicator.setText(String.valueOf(this.counter));
+        if (this.useVibration) vibrate();
     }
+
 
     private void decreaseCounter() {
         if (this.counter > 0) this.counter--;
         this.indicator.setText(String.valueOf(this.counter));
+        if (this.useVibration) vibrate();
+    }
+
+    private void vibrate() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v.vibrate(VibrationEffect.createOneShot(VIBRATION_TIME, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            // NOTE: deprecated in API 26
+            v.vibrate(VIBRATION_TIME);
+        }
     }
 }
--- app/src/main/java/com/om/counter/SettingsActivity.java
+++ app/src/main/java/com/om/counter/SettingsActivity.java
@@ -1,31 +1,34 @@
 package com.om.counter;
 
 import android.os.Bundle;
-
+import android.view.MenuItem;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceFragmentCompat;
+import androidx.core.app.NavUtils;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.settings, new SettingsFragment())
-                .commit();
-        ActionBar actionBar = getSupportActionBar();
+        setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = this.getSupportActionBar();
+
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
-        @Override
-        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-            setPreferencesFromResource(R.xml.preferences, rootKey);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
         }
+        return super.onOptionsItemSelected(item);
     }
+
+
+
 }
