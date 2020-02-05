81c88807947b756484b834fc2a7b4b51ebb86bf7
Maxim Obrubov <m.obrubov@yandex.ru>
2020-02-05 11:02:23 +0400

    using preference fix

--- app/src/main/java/com/om/counter/MainActivity.java
+++ app/src/main/java/com/om/counter/MainActivity.java
@@ -1,9 +1,6 @@
 package com.om.counter;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -22,11 +19,11 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener{
 
-    int VIBRATION_TIME = 100;
-    int counter = 0;
-    boolean useHardware;
-    boolean useVibration;
-    TextView indicator;
+
+    private int VIBRATION_TIME = 50;
+    private int counter = 0;
+    private SharedPreferences prefs;
+    private TextView indicator;
 
     /**
      * Increase counter from a view
@@ -54,17 +51,16 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     public boolean dispatchKeyEvent(KeyEvent event) {
         int action = event.getAction();
         int keyCode = event.getKeyCode();
-
-        Toast.makeText(this, String.valueOf(this.useHardware), Toast.LENGTH_SHORT).show();
+        boolean useHardware = this.prefs.getBoolean("hardware_buttons", true);
 
         switch (keyCode) {
         case KeyEvent.KEYCODE_VOLUME_UP:
-            if (action == KeyEvent.ACTION_UP && this.useHardware) {
+            if (action == KeyEvent.ACTION_UP && useHardware) {
                 increaseCounter();
             }
             return true;
         case KeyEvent.KEYCODE_VOLUME_DOWN:
-            if (action == KeyEvent.ACTION_DOWN && this.useHardware) {
+            if (action == KeyEvent.ACTION_DOWN && useHardware) {
                 decreaseCounter();
             }
             return true;
@@ -101,44 +97,34 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         setContentView(R.layout.activity_main);
         this.indicator = (TextView) findViewById(R.id.textview);
         setupSharedPreferences();
-
-        Toast.makeText(this, "On create", Toast.LENGTH_SHORT).show();
     }
 
 
     private void setupSharedPreferences() {
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
-
-//        TODO: check here to find why options are reset
+        if (this.prefs == null) {
+            this.prefs = PreferenceManager.getDefaultSharedPreferences(this);
+            this.prefs.registerOnSharedPreferenceChangeListener(this);
+        }
     }
 
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-
-        if (key.equals("hardware_buttons")) {
-            this.useHardware = sharedPreferences.getBoolean("hardware_buttons", true);
-            Toast.makeText(this, key + " " + sharedPreferences.getBoolean("hardware_buttons", true) + " " + this.useHardware, Toast.LENGTH_SHORT).show();
-        } else if (key.equals("vibration")) {
-            Toast.makeText(this, key + sharedPreferences.getBoolean("vibration", true), Toast.LENGTH_SHORT).show();
-            this.useVibration = sharedPreferences.getBoolean("vibration", true);
-        }
-
+        this.prefs = sharedPreferences;
     }
 
 
     private void increaseCounter() {
         this.counter++;
         this.indicator.setText(String.valueOf(this.counter));
-        if (this.useVibration) vibrate();
+        if (prefs.getBoolean("vibration", true)) vibrate();
     }
 
 
     private void decreaseCounter() {
         if (this.counter > 0) this.counter--;
         this.indicator.setText(String.valueOf(this.counter));
-        if (this.useVibration) vibrate();
+        if (prefs.getBoolean("vibration", true)) vibrate();
     }
 
     private void vibrate() {
